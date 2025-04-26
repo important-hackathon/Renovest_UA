a4c10451d9807bcf67bb643f22b69d4d02232238
Ostap Sidoryak <ostapsidorak@gmail.com>
2025-04-26 20:32:10 +0300

    feature/JoinMovement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AboutUs from "@/components/home/AboutUs";
+import JoinMovement from "@/components/home/JoinMovement";
 import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <AboutUs />
+      <JoinMovement />
     </div>
   );
 }
