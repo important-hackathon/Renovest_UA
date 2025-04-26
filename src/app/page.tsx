1bbeb250d4066ca371ecf28d14cddfb23a12130b
Vadym <vadymzastavniy@gmail.com>
2025-04-26 18:05:19 +0300

    feat: add why us section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import AboutUs from "@/components/home/AboutUs";
 import { Navbar } from '@/components/Navbar';
-
+import WhyUs from "@/components/home/WhyUs";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <AboutUs />
+      <WhyUs />
     </div>
   );
 }
